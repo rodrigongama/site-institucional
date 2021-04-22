7d2b6662e399527110f52d3aadcf4801d965ead5
Rodrigo Gama <rodrigongama.adv@gmail.com>
2021-04-21 21:57:03 -0300

    adicionando carrossel

--- static/js/main.js
+++ static/js/main.js
@@ -1,27 +1,98 @@
-let nav = document.querySelector('.nav-top')
-let menu = document.querySelector('.side-menu')
-let closeMenu = document.querySelector('.close-menu')
+const Navigation = {
+    init: function (){
+        this.addListeners();
+    },
+    addListeners: function(){
+        let nav = $('.nav-top')
+        let menu = $('.side-menu')
+        let closeMenu = $('.close-menu')
 
-nav.addEventListener('click', event => {
-    menu.classList.remove('hidden')
-}) 
+        nav.on('click', event => {
+            menu.removeClass('hidden')
+        }) 
 
-closeMenu.addEventListener('click', event => {
-    menu.classList.add('hidden')
-})
+        closeMenu.on('click', event => {
+            menu.addClass('hidden')
+        })
+    }
+}
 
+const Float = {
+    init: function(){
+        this.addSlick();
+    },
+    addSlick: function(){
+        $('.section-float').slick({
+            infinite: false,
+            autoplay: true,
+            autoplaySpeed: 2500,
+        });
+    }
+}
 
-let news = document.querySelector('.float-news')
-let open = document.querySelector('.news-open')
-let closeNews = document.querySelector('.close-news')
-let layer = document.querySelector('.layer')
+const News = {
+    init: function(){
+        this.addListeners();
+    },
+    addListeners: function(){
+        let news = $('.float-news')
+        let open = $('.news-open')
+        let closeNews = $('.close-news')
+        let layer = $('.layer')
 
-news.addEventListener('click', event => {
-    open.classList.remove('news-hidden')
-    layer.classList.add('layer-active')
-})
+        news.on('click', event => {
+            open.removeClass('news-hidden')
+            layer.addClass('layer-active')
+        })
 
-closeNews.addEventListener('click', event => {
-    open.classList.add('news-hidden')
-    layer.classList.remove('layer-active')
-})
+        closeNews.on('click', event => {
+            open.addClass('news-hidden')
+            layer.removeClass('layer-active')
+        })
+    }
+}
+
+const Paterns = {
+    init: function(){
+        this.addSlick();
+    },
+    addSlick: function(){
+        $('.partners').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2500,
+            responsive: [
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+                  infinite: true,
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+              
+            ]
+        });
+    }
+}
+
+Navigation.init();
+Float.init();
+News.init();
+Paterns.init();
